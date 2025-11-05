a2089103e26a17fab334c844b5d900e670cbcab1
jangsion23 <sionjang23@naver.com>
2025-11-05 14:42:13 +0900

    summary not complit

--- JOLUV/src/App.tsx
+++ JOLUV/src/App.tsx
@@ -2,32 +2,26 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout'; // 1. Layout이 import 되어 있는지
-
-// 2. 모든 페이지들이 import 되어 있는지
+import Layout from './components/Layout.tsx'; // 👈 1. Layout을 import
 import MainPage from './pages/main/Mainpage.tsx';
 import LoginPage from './pages/login/index.tsx';
 import SignupPage from './pages/login/signup.tsx';
 import ChecklistPage from './pages/checklist/index.tsx';
-
-// 👇 3. SummaryPage가 import 되어 있는지 확인하세요!
-import SummaryPage from './pages/summary/index.tsx'; 
+import SummaryPage from './pages/summary/index.tsx';
 
 function App() {
   return (
     <Routes>
+      {/* 2. Layout 컴포넌트가 모든 자식 경로를 감싸도록 설정 */}
       <Route path="/" element={<Layout />}>
-        {/* Layout 안의 모든 페이지 경로 */}
-        <Route index element={<MainPage />} />
+        {/* 👇 여기에 있는 페이지들은 모두 Header를 갖게 됩니다. */}
+        <Route index element={<MainPage />} /> {/* 👈 path="/"와 동일 */}
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
         <Route path="checklist" element={<ChecklistPage />} />
-        
-        {/* 👇 4. /summary 경로가 <Route>에 등록되어 있는지 확인하세요! */}
         <Route path="summary" element={<SummaryPage />} />
+        {/* (다른 페이지 경로들도 여기에 추가) */}
       </Route>
-
-      {/* Layout 밖의 페이지 (헤더가 없는 페이지) */}
-      <Route path="login" element={<LoginPage />} />
-      <Route path="signup" element={<SignupPage />} />
     </Routes>
   );
 }
