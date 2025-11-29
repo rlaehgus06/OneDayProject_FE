50ba7ed682e9a0aef9ecff5dcde227b82ac046db
rlaehgus06 <6uf3rw@naver.com>
2025-11-29 23:44:52 +0900

    vercel2

--- JOLUV/src/components/ProtectedRoute.tsx
+++ JOLUV/src/components/ProtectedRoute.tsx
@@ -7,7 +7,6 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { isLoggedIn } = useAuth();
-
   // 로그인이 되어 있으면 원래 보여주려던 페이지(children)를 보여줍니다.
   return <>{children}</>;
 };
